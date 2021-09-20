2396075aeb59addf463ac04bba010ccb1a97cde6
Peter Ramos <steinbergcurse@gmail.com>
2021-09-21 00:16:27 +0800

    - Added letter count support for letters.js - Nodemon ignores public folder - Added basic functionalities to ui

--- letters.js
+++ letters.js
@@ -17,3 +17,13 @@ function recurse(inp, point = "", form = "") {
 }
 recurse("staged");
 console.log(samplelist);
+
+const critical = samplelist.filter((i) => i.length === 6);
+const heavy = samplelist.filter((i) => i.length === 5);
+const normal = samplelist.filter((i) => i.length === 4);
+const half = samplelist.filter((i) => i.length === 3);
+
+console.log("CRITICAL", critical.length, critical);
+console.log("HEAVY", heavy.length, heavy);
+console.log("NORMAL", normal.length, normal);
+console.log("HALF", half.length, half);
--- public/index.js
+++ public/index.js
@@ -1,1 +1,29 @@
 const socket = io();
+const STATE = { word: "" };
+const wordDisplay = document.getElementById("word-display");
+
+function addLetter(e) {
+  if (wordDisplay.children.length === 6) return;
+  const text = e.innerText;
+  STATE.word = STATE.word + text.toLowerCase();
+  wordDisplay.innerHTML += `<div class="btn-floating btn-small">${text.toUpperCase()}</div>`;
+}
+
+function clearDisplay() {
+  wordDisplay.innerHTML = "";
+  STATE.word = "";
+}
+
+function deleteDisplay() {
+  wordDisplay.removeChild(wordDisplay.lastElementChild);
+  STATE.word = STATE.word.slice(0, -1);
+}
+
+document.addEventListener("keyup", (e) => {
+  if (e.keyCode === 8) return deleteDisplay();
+  const letterList = document.querySelectorAll("#letter-btn");
+  letterList.forEach((element) => {
+    if (element.innerText.toLowerCase() === e.key.toLowerCase())
+      addLetter(element);
+  });
+});
